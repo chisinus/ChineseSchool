cda0a0efa68493211a9bdb9f9034319d47d0f214
xiajun <chisinus@gmail.com>
2015-04-27 01:18:35 -0400

    1

---
+++ ChineseSchool/BusinessObjects/GradeData.cs
@@ -0,0 +1,35 @@
+﻿using ChineseSchool.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ChineseSchool.BusinessObjects
+{
+    public class GradeData : CSObject, ICSObjectFromDB
+    {
+        #region properties
+        public int GradeID { get; set; }
+        public string GradeName { get; set; }
+        #endregion properties
+
+        #region constructor
+        public GradeData(SqlDataReader reader)
+        {
+            GetDataFromDB(reader);
+        }
+
+        public GradeData()
+        {
+            // TODO: Complete member initialization
+        }
+        #endregion constructor
+
+        public void GetDataFromDB(SqlDataReader reader)
+        {
+            GradeID = Toolbox.GetDBValue<int>(reader, "GradeID");
+            GradeName = Toolbox.GetDBValue<string>(reader, "GradeName");
+        }
+    }
+}
